c6231a1c44aa82e5104a33484d047f2f2f8035e8
Dairo Parra <parra.dairo@gmail.com>
2022-01-01 23:48:52 -0500

    Tailwindcsss add feature

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>@yield('title') - Laravel App</title>
-
-    <!-- Tailwind CSS Link -->
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
+    <title>@yield('title') - Reto</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body class="bg-gray-100 text-gray-800">
+<body class="bg-gray-100 dark:bg-gray-900 dark:text-white text-gray-800">
 <!-- Document body -->
 
     <nav class="flex py-5 bg-blue-600 text-white">
